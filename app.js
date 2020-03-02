76add08096f6e7bf34171efdd4e51e42da146458
IsisPMR <piorctt@gmail.com>
2020-03-02 11:18:05 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+function helloWorld(name){
+    console.log(`Hello ${name}`);
+}
+
+const hello  = (name) => console.log(`Hello ${name}`);
+helloWorld('Isis');
+hello("Isis Paulina");
+*/
+
+
+const p = {name: 'Isis', 
+           age:'28'};
+
+console.log(p);
